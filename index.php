b454cee1a073a5688baf22f0e8569c23cc7c655a
teodor-ionesco <31542280+teodor-ionesco@users.noreply.github.com>
2018-08-28 17:36:01 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Welcome! || mau5</title>
+		<!--link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-rc.2/css/materialize.min.css">	-->
+		<style>
+			body 
+			{
+				background-image: url(static/img/imagedoc-darknoise.png);
+			}
+			
+			header
+			{
+				box-shadow: 0px 8px 8px #8a1f1f;
+				border-radius: 10px;
+				text-align: center;
+			}
+			
+			header div
+			{
+				color: yellow;
+				font-family: "Chiller", "Sans-Serif";
+				font-size: 88px;
+				color: #ffff00;
+			}
+			
+			main
+			{
+				margin: 50px;
+				text-align: center;
+			}
+			
+			a 
+			{
+				color: forestgreen;
+				line-height: 2;
+				font-size: 30px;
+				font-family: "Comic Sans MS", cursive, sans-serif;
+				text-decoration: none;
+			}
+			
+			a:hover
+			{
+				text-decoration: underline;
+				color: red;
+			}
+			
+			a:active
+			{
+				color: brown;
+			}
+		</style>
+	</head>
+	
+	
+	<body title="The gateway to a deadmau5 fan.">
+		<header>
+			<div>mau5portal</div>
+		</header>
+		
+		<main>
+			<a href="#" title="The one I like">My personalized website</a>
+			<br>
+			<a href="#" title="My website built for employment and official stuff">My official website</a>
+			<br>
+			<br>
+			<img src="static/img/b04.gif" width="200">
+		</main>
+		
+		<footer>
+		</footer>
+		<!--script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-rc.2/js/materialize.min.js"></script>-->
+		<script src="static/js/jquery.min.js"></script>
+	</body>
+</html>

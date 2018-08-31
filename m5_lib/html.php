f93ce604269994f3722bc742e3830f00557234d6
teodor-ionesco <31542280+teodor-ionesco@users.noreply.github.com>
2018-08-31 19:25:51 +0300

    Fixed a URL typo

--- m5_lib/html.php
+++ m5_lib/html.php
@@ -27,7 +27,7 @@ class HTML
 	{
 		print('
 			<div id="cover" style="background-color: #232323; position: absolute; width: 100%; height: 100%;">
-				<img id="cover_img" hidden="" style="position: absolute;" src="//<?= SITE_PORTAL_URL ?>/static/img/spinner.gif" width="10%">
+				<img id="cover_img" hidden="" style="position: absolute;" src="//'.SITE_PORTAL_URL.'/static/img/spinner.gif" width="10%">
 			</div>
 		');
 	}

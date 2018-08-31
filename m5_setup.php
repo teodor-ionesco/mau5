f4b0ac95d5a5e6453e7b96076b9fedc5af188461
teodor-ionesco <31542280+teodor-ionesco@users.noreply.github.com>
2018-08-31 03:28:02 +0300

    Improved setup

--- m5_setup.php
+++ m5_setup.php
@@ -5,6 +5,8 @@ if(file_exists('m5_setup_data.php'))
 	file_put_contents('m5_lib/config.php', file_get_contents('m5_setup_data.php'));
 
 	unlink('m5_setup_data.php');
+	
+	die('Setup is done.');
 }
 else
 {

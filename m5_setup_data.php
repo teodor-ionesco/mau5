f728a7d20921bf7ceccb02702f1dba055052077b
teodor-ionesco <31542280+teodor-ionesco@users.noreply.github.com>
2018-09-01 16:22:53 +0300

    Fixes

--- m5_setup_data.php
+++ m5_setup_data.php
@@ -2,3 +2,8 @@
 
 define('SITE_PORTAL_URL', 'mau5.ml');
 define('SITE_HOBBY_URL', 'personal.mau5.ml');
+
+define('DB_USR', 'root');
+define('DB_PSS', 'root');
+define('DB_HST', '127.0.0.1');
+define('DB_NAM', 'mau5');

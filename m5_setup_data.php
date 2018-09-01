256297b5e488e45ca7486d9e41b2cba4d27cecd6
teodor-ionesco <31542280+teodor-ionesco@users.noreply.github.com>
2018-09-01 16:26:38 +0300

    Updates

--- m5_setup_data.php
+++ m5_setup_data.php
@@ -3,7 +3,7 @@
 define('SITE_PORTAL_URL', 'mau5.ml');
 define('SITE_HOBBY_URL', 'personal.mau5.ml');
 
-define('DB_USR', 'root');
-define('DB_PSS', 'root');
-define('DB_HST', '127.0.0.1');
-define('DB_NAM', 'mau5');
+define('DB_USR', 'u428832643_m5');
+define('DB_PSS', 'IW:LK$&>tr#iB/uLW5');
+define('DB_HST', 'localhost');
+define('DB_NAM', 'u428832643_mau5');

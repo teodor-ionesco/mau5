b332395ad4af5a85b8ddd37112a9a7100a0c7f3d
teodor-ionesco <31542280+teodor-ionesco@users.noreply.github.com>
2018-10-02 21:16:40 +0300

    Added geography quiz system.

---
+++ geogra/ro/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>România - Index</title>
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	</head>
+
+	<body>
+		<div class="container">
+			<p class="flow-text">Bună! Ce doriți să repetați?</p>
+			<ol class="flow-text">
+				<li><a href="relief/unitati/index.php">Unități de relief</a></li>
+				<li><a href="romania.php">Subunități de relief</a></li>
+			</ol>
+		</div>
+		
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+	</body>
+</html>
---
+++ geogra/ro/relief/unitati/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Unități - Index</title>
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	</head>
+
+	<body>
+		<div class="container">
+			<p class="flow-text">Bună! Ce doriți să repetați?</p>
+			<ol class="flow-text">
+				<li><a href="dcat/teorie.php">Depresiunea Colinară a Transilvaniei</a></li>
+				<!--<li><a href="romania.php">Subunități de relief</a></li>-->
+			</ol>
+		</div>
+		
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+	</body>
+</html>

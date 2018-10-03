2ca56dc29418d539d0fba52091c5d375292bb7ed
Teodor <31542280+teodor-ionesco@users.noreply.github.com>
2018-10-03 10:58:23 +0300

    Create teorie.php

---
+++ geogra/ro/relief/unitati/poddob/teorie.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Podişul Dobrogei - Teorie</title>
+		
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+		
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	</head>
+
+	<body>
+		<div class="container">
+			<h4>Podişul Dobrogei</h4>
+			
+			<div class="divider"></div> 
+			
+			<p class="flow-text">
+				► <b>Geneza: </b> 
+          &nbsp;&nbsp;&nbsp;• Masivul Nord-Dobrogean <i>(Nord)</i>: în timpul orogenezei <span style="background-color:yellow;">hercinice</span> prin <span style="background-color:yellow;">încreţirea</span> scoarţei terestre.<br>
+				  &nbsp;&nbsp;&nbsp;• Podişul Casimcei <i>(Centru)</i>: în timpul orogenezei <span style="background-color:yellow;">caledoniene</span>, prin <span style="background-color:yellow;">încreţirea</span> scoarţei terestre.<br>
+          &nbsp;&nbsp;&nbsp;• Podişul Dobrogei de Sud <i>(Sud)</i>: prin <span style="background-color:yellow;">sedimentare</span>
+				<br><br>
+				
+				► <b>Petrografie: </b> 
+          &nbsp;&nbsp;&nbsp;• Masivul Nord-Dobrogean: în timpul orogenezei <span style="background-color:yellow;">hercinice</span> prin <span style="background-color:yellow;">încreţirea</span> scoarţei terestre.<br>
+				  &nbsp;&nbsp;&nbsp;• Podişul Casimcei <i>(Centru)</i>: în timpul orogenezei <span style="background-color:yellow;">caledoniene</span>, prin <span style="background-color:yellow;">încreţirea</span> scoarţei terestre.<br>
+          &nbsp;&nbsp;&nbsp;• Podişul Dobrogei de Sud <i>(Sud)</i>: prin <span style="background-color:yellow;">sedimentare</span>
+				<br><br>
+				
+				► <b>Relieful <i>(în genere)</i>: </b> 
+				<br>
+					&nbsp;&nbsp;&nbsp;• Altitudini medii: <span style="background-color:yellow;">300-500 m</span><br>
+					&nbsp;&nbsp;&nbsp;• Altitudini maxime: <span style="background-color:yellow;">682 m</span> (Dealul Ciungi)<br>
+					&nbsp;&nbsp;&nbsp;• Fragmentare: <span style="background-color:yellow;">deluroasă</span> și <span style="background-color:yellow;">colinară</span>
+				<br><br>
+				
+				► <b>Relieful fluvial: </b> 
+				<span style="background-color:yellow;">văi largi</span> însoţite de <span style="background-color:yellow;">terase</span>
+				<br><br>
+				
+				► <b>Informații adiționale: </b>
+					<br>
+					&nbsp;&nbsp;&nbsp;• La contactul dintre Câmpia Moldovei şi unităţi mai înalte <i>(ex: Pod. Sucevei)</i> se formează <span style="background-color:yellow;">cueste</span>.<br>
+					&nbsp;&nbsp;&nbsp;• Are o înclinare generală de la N la S.
+      </p>
+			<a href="quiz.php" class="btn waves-effect waves-light red">Quiz</a>
+			<br>
+			<br>
+		</div>
+		
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+	</body>
+</html>

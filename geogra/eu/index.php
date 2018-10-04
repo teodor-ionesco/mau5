65279a8a6bf9cbbd99f8eb965984931f07a83f24
Teodor <31542280+teodor-ionesco@users.noreply.github.com>
2018-10-04 10:53:21 +0300

    Create index.php

---
+++ geogra/eu/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Europa - Index</title>
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	</head>
+
+	<body>
+		<div class="container">
+			<p class="flow-text">Bună! Ce doriți să repetați?</p>
+			<ol class="flow-text">
+				<li><a target="_BLANK" href="//www.geogra.ro/joc/europa_state/index.php">Statele Europei</a></li>
+				<li><a target="_BLANK" href="//www.geogra.ro/joc/europa_capitale/index.php">Capitalele Europei</a></li>
+        <li><a target="_BLANK" href="//www.geogra.ro/joc/ro/eumunti/index.php">Munţii Europei</a></li>
+        <li><a target="_BLANK" href="//www.geogra.ro/joc/ro/eupod/index.php">Podişurile Europei</a></li>
+        <li><a target="_BLANK" href="//www.geogra.ro/joc/ro/eucamp/index.php">Câmpiile Europei</a></li>
+        <li><a target="_BLANK" href="//www.geogra.ro/joc/europa_rauri/index.php">Râurile Europei</a></li>        
+        <li><a target="_BLANK" href="//www.geogra.ro/joc/ro/euinspen/index.php">Insule şi peninsule din Europa</a></li>
+        <li><a target="_BLANK" href="//www.geogra.ro/joc/ro/eumari/index.php">Mări, golfuri, strâmtori din Europa</a></li>
+			</ol>
+		</div>
+		
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+	</body>
+</html>
